fe4c2453ba8593821b82e913257c3b4138b4086f
Daniel Chabr <danielchabr@gmail.com>
2014-03-23 15:21:59 +0100

    Manifest changes, ScanCode Activity added

---
+++ app/src/main/java/com/bibliofair/glass/ScanCodeActivity.java
@@ -0,0 +1,17 @@
+package com.bibliofair.glass;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+
+public class ScanCodeActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_code);
+
+    }
+
+
+}
