ea6303563a80d9c5ce5493269365da8e506047fc
Daniel Chabr <danielchabr@gmail.com>
2014-03-23 16:37:12 +0100

    MainActivity intent for ScanCode, show result in Card

--- app/src/main/java/com/bibliofair/glass/MainActivity.java
+++ app/src/main/java/com/bibliofair/glass/MainActivity.java
@@ -1,65 +1,56 @@
 package com.bibliofair.glass;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
-public class MainActivity extends ActionBarActivity {
+import com.google.android.glass.app.Card;
+
+
+public class MainActivity extends Activity {
+    static final int GET_BOOK_ISBN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        Intent scanCodeIntent = new Intent(this, ScanCodeActivity.class);
+        startActivityForResult(scanCodeIntent, GET_BOOK_ISBN);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == GET_BOOK_ISBN) {
+            if (resultCode == Activity.RESULT_OK) {
+                String text = data.getStringExtra(ScanCodeActivity.EXTRA_TEXT);
+                if (isValidISBN(text)) {
+                    showStatus(R.string.valid_code, text);
+                } else {
+                    showStatus(R.string.invalid_code, "");
+                }
+            } else {
+                showStatus(R.string.scanning_failed, "");
+            }
         }
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+    private boolean isValidISBN (String text) {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
+    private void showStatus (int statusCode, String text) {
+        String status = this.getString(statusCode);
+        Card statusCard = new Card(this);
+        if (text.equals("")) {
+            statusCard.setText(status);
+        } else {
+            statusCard.setText(status + " " + text);
         }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
+        View cardView = statusCard.toView();
+        setContentView(cardView);
     }
 
 }
